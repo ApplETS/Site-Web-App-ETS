29b82a0d0884905f4c14773f4dfedabc0d13d7a1
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-07-30 23:42:57 -0400

    Added optionals cursor removal option to typewriter

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,23 +1,36 @@
 /**
  * A typewriter that dynamically emulates the input of a user
- * @param {*} iterationSpeed The speed at which the text is typed
- * @param {*} destinationElement The HTML element in which the inner text will be inserted
- * @param {*} inputText The text to type
- * @param {*} callback (Optional) The function to execute once the typing is done
+ * @param {Int32Array} iterationSpeed The speed at which the text is typed
+ * @param {Element} destinationElement The HTML element in which the inner text will be inserted
+ * @param {String} inputText The text to type
+ * @param {Bool} removeCursor Remove cursor at the end
+ * @param {function} callback (Optional) The function to execute once the typing is done
  */
-var Typewriter = function(iterationSpeed, destinationElement, inputText,callback){
+var Typewriter = function(iterationSpeed, destinationElement, inputText,cursorToRemove, callback){
     iterateTypewriter = function(iterationSpeed,destinationElement,callback) {
         return function(inputText,iTextPos){
             
             iArrLength = inputText.length
             
+            iTextPos++
             
     
             
             destinationElement.innerText = inputText.substring(0, iTextPos);
-            if (iTextPos++ == iArrLength) {
-    
-                setTimeout(callback, 1000)
+            if (iTextPos == iArrLength) {
+                if(cursorToRemove == true){
+                    setTimeout("removeCursor()",1000)
+                    if(callback != undefined){
+                        callback()
+                    }
+                }else{
+                    if(callback != undefined){
+                        callback()
+                    }
+                }
+                
+                
+                
                 
             } else {
                 
@@ -25,9 +38,16 @@ var Typewriter = function(iterationSpeed, destinationElement, inputText,callback
         
                 
             }
+            
         }
+
+        
         
     }(iterationSpeed,destinationElement,callback)
+
+    removeCursor = function(){
+        destinationElement.classList.remove("typewriter")
+    }
     
     setTimeout("iterateTypewriter(\"" + inputText.replace("\"", "") + "\"," + 0  +  ")", 1000);
 }
