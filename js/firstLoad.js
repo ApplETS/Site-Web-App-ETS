5bcd551798a111d82a0b3cd36873ab99a25c8d92
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-08-08 23:06:12 -0400

    Remove local storage variable
    Variable "codeHasLoadedBefore" caused problems with Safari and was not necessary to the functionning of the website.

--- js/firstLoad.js
+++ js/firstLoad.js
@@ -1,34 +1,26 @@
-if(localStorage.getItem("codeHasLoadedBefore") === null){
-    $('#navButtons').hide()
-    $('#navTitle').toggle()
-    $('#nav-toggle').css('opacity', '0')
-    
-    var y = Number(screen.height / 2) - Number($('#brandImage').position().top) - Number($('#brandImage').height())
-    document.getElementById("brandImage").style["transform"] = "translateY(" +  Math.round(y) + "px"+ ")"
+$('#navButtons').hide()
+$('#navTitle').toggle()
+$('#nav-toggle').css('opacity', '0')
 
-    $('#navTitle').fadeToggle(1000, "swing",function(){
-        
-        document.getElementById("brandImage").classList.add("brandImageTop")
-        
-        setTimeout("showNavButtons()",1000)
+var y = Number(screen.height / 2) - Number($('#brandImage').position().top) - Number($('#brandImage').height())
+document.getElementById("brandImage").style["transform"] = "translateY(" +  Math.round(y) + "px"+ ")"
+
+$('#navTitle').fadeToggle(1000, "swing",function(){
+    
+    document.getElementById("brandImage").classList.add("brandImageTop")
+    
+    setTimeout("showNavButtons()",1000)
+})
+
+
+
+var showNavButtons = function(){
+    $('#nav-toggle').css('opacity', '1')
+    $('#navButtons').show("slow","swing",function(){
+
     })
-    
-    
-    
-    var showNavButtons = function(){
-        $('#nav-toggle').css('opacity', '1')
-        $('#navButtons').show("slow","swing",function(){
-
-        })
-    }
-    
-    document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
-        this.classList.toggle( "active" );
-    });
-
 }
 
-
-
-
-
+document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+});
