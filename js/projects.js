43243632bf5ab55670a97cf3f4ade6743a619e0b
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-08-15 23:11:43 -0400

    Test lag-removal by executing whole function in steps

--- js/projects.js
+++ js/projects.js
@@ -4,11 +4,14 @@ $('#titleDiv').fadeToggle(1500,"swing")
 $(function() {
 
     $(window).scroll(function() {
-        if($(this).scrollTop() > 0){
-            var mass = Number(1 + 0.2*$(this).scrollTop());
+        if($(this).scrollTop() > 0 ){
+            if($(this).scrollTop() % 10 === 0){
+                var mass = Number(1 + 0.2*$(this).scrollTop());
   
-            $('#projectHeart').css({'transform': 'translate3d(-50%,-50%,0) scale(' + mass + ')'});
-            console.log($('#projectHeart').css('transform'), window.innerWidth,window.innerHeight)
+                $('#projectHeart').css({'transform': 'translate3d(-50%,-50%,0) scale(' + mass + ')'});
+                console.log($('#projectHeart').css('transform'), window.innerWidth,window.innerHeight)
+            }
+           
         }else{
             $('#projectHeart').css({'transform': 'scale(1) translate3d(-50%,-50%,0)'});
         }
