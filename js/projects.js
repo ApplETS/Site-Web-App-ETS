426663a6d93f9556f7f25a1ffb216734bc698ced
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-08-26 16:42:28 -0400

    Add device mockups and initial layout

--- js/projects.js
+++ js/projects.js
@@ -22,12 +22,14 @@ var iScrollPos = 0
   });*/
 
 $('#fullpage').fullpage({
+    autoScrolling:true,
+    scrollOverflow:true,
     onLeave: function(index,nextIndex,direction){
         if(index.index==0 && nextIndex.index == 1 && direction ==="down"){
-
             $('#projectHeart').toggleClass("heartbeat")
             $('#projectHeart').css("width", "600vh")
             $('.fp-controlArrow').hide()
+
             
             
         }
@@ -38,29 +40,44 @@ $('#fullpage').fullpage({
             $('#projectHeart').css("width", "4vh")
             $('.project-slide').fadeToggle(500,"linear")
 
+            $('#header').css('background-color', '')
+
             
         }
     },
     afterLoad : function(anchorLink, index){
         console.log(index)
         if(index.index == 1){
+            $.fn.fullpage.setAutoScrolling(true)
+
             $('#section1').css("background-color", "#EC2B27")
             $('#projectHeart').css("display", "none")
 
             $('.project-slide').fadeToggle(500,"swing")
             $('.fp-controlArrow').fadeToggle(500,"swing")
+
+            $('#androidETSMOBILE').css('height', '45vh')
+            $('#play-store-link').css('width', '165px')
+
+            $('#header').css('background-color', '#3e3e3e')
+
         }
         if(index.index == 0){
             $('#projectHeart').toggleClass("heartbeat")
         }
         
         
+    },
+    afterSlideLoad: function(section,origin,destination,direction){
+        console.log(section,origin,destination,direction)
     }}
 
     
 
     
 )
+
+fullpage_api.setAllowScrolling(true);
 
 
 
@@ -73,5 +90,21 @@ $('#projectHeart').on('click', function(){
 document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
     this.classList.toggle( "active" );
 });
+
+$('#android-etsmobile-link').on("mouseover",function(){
+    $('#androidETSMOBILE').css('height', '45vh')
+    $('#play-store-link').css('width', '165px')
+    $('#iosETSMOBILE').css('height', '35vh')
+    $('#app-store-link').css('width', '100px')
+})
+
+
+$('#ios-etsmobile-link').on("mouseover",function(){
+    $('#iosETSMOBILE').css('height', '45vh')
+    $('#app-store-link').css('width', '165px')
+    $('#androidETSMOBILE').css('height', '35vh')
+    $('#play-store-link').css('width', '100px')
+})
+
 
 
