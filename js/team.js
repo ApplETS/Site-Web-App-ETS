8a2de4677d9628f9908c5edb5eb03b87e0e614b1
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-10-01 15:28:53 -0400

    Add fade intro

--- js/team.js
+++ js/team.js
@@ -1,6 +1,8 @@
 $('#row1').hide();
 $('#row2').hide();
-$('#row3').hide()
+$('#row3').hide();
+$('#intro-title').hide();
+$('#intro-title').fadeToggle(1000,"swing");
 $('#row1').toggle("slide",1500);
 $('#row2').toggle("slide",{direction:'right'},1500);
 $('#row3').toggle("slide",1500);
