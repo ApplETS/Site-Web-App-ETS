110906c8fb6206b54ad98b7c9366ae943a5d94fa
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-10-06 13:38:35 -0400

    Add partners page content

---
+++ js/partners.js
@@ -0,0 +1,17 @@
+document.querySelector("#floatingToggle").addEventListener("click", function () {
+
+
+    document.querySelector("#nav-toggler").classList.toggle("active");
+    $('#navToggleContent').slideToggle()
+    $('#navbar').toggleClass("back-color-black")
+
+
+});
+
+window.onscroll = function(){
+    if(this.document.getElementById("main-content").getBoundingClientRect().y < 0){
+        $('#navbar').addClass("back-color")
+    }else{
+        $('#navbar').removeClass("back-color")
+    }
+}
