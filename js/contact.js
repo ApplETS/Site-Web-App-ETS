4e003f37d09d966d0014facfefb66e0f32e92a6e
Emmanuel Proulx <foufoulefou2juillet@hotmail.com>
2018-10-14 16:32:53 -0400

    Add fanelle and finalize corrections

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,7 @@
 $('#member-title').hide();
 $('#partner-title').hide();
 $('.member-form-content').hide();
+$('.partner-form-content').hide();
 
 $('#member-title').toggle("fade",1500);
 $('#partner-title').toggle("fade",1500);
@@ -31,8 +32,21 @@ $('#member-contact').on('click', function () {
     setTimeout("showMemberForm()",500)
 })
 
+$('#partner-contact').on('click', function () {
+    
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#form-container").offset().top-100
+    }, 1000);
+    setTimeout("showPartnerForm()",500)
+})
+
 function showMemberForm(){
+    $('.partner-form-content').hide()
     $('.member-form-content').show("blind")
+}
+function showPartnerForm(){
+    $('.member-form-content').hide()
+    $('.partner-form-content').show("blind")
 }
 
 window.onscroll = function(){
--- js/team.js
+++ js/team.js
@@ -1,13 +1,15 @@
 $('#row1').hide();
 $('#row2').hide();
-$('#row3').hide();
+//$('#row3').hide();
 $('#intro-title').hide();
 $('#intro-title').fadeToggle(1000,"swing");
 $('#row1').toggle("slide",1500);
-if(screen.height > 992){
+if(!isTouchScreen){
     $('#row2').show("slide",{direction:'right'},1500);
-    $('#row3').show("slide",1500);
+    //$('#row3').show("slide",1500);
 }
+
+var isTouchScreen = false;
 
 
 
@@ -29,17 +31,22 @@ window.onscroll = function(){
     }else{
         $('#navbar').removeClass("back-color")
     }
-    if(screen.height < 992){
+    if(isTouchScreen){
         if(this.document.getElementById("row1").getBoundingClientRect().y < 0){
             $('#row2').show("slide",{direction:'right'},1500);
         }
     
         if(this.document.getElementById("row2").getBoundingClientRect().y < 0){
-            $('#row3').show("slide",1500);
+           // $('#row3').show("slide",1500);
         }
     }
 
 }
+
+//In case the device is a touchscreen
+document.addEventListener('touchstart', function () {
+    isTouchScreen = true;
+})
 
 
 
