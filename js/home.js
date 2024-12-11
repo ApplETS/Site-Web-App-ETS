3b73ad788c83a0524735280dcbc0f2c71bada5f6
Louis-Philippe <louis.philippe.heon@gmail.com>
2024-12-10 21:40:31 -0500

    Update homepage

--- js/home.js
+++ js/home.js
@@ -1,13 +1,4 @@
 /**
- * Initialise parallax (for the android picture)
- */
-$('#about').parallax({
-    imageSrc: 'images/droidcon.webp',
-    speed:0.1,
-    positionY:"top"
-});
-
-/**
  * Fade-in the title and buttons
  */
 $('#navTitle').fadeToggle(1000, "swing", function () {
@@ -17,48 +8,11 @@ $('#navTitle').fadeToggle(1000, "swing", function () {
 /**
  * Fadein function
  */
-var showNavButtons = function () {
+let showNavButtons = function () {
     $('#nav-toggler').css('opacity','1')
     $('#navButtons').show("slow", "swing", function () {
         $('#link-buttons-container').css('opacity','1')
         $("#typewriter-container").toggleClass("typewriter")
-        new Typewriter(70, document.getElementById("typewriter-container"), "Pour ceux qui partagent la passion du développement mobile.", false, null)
+        new Typewriter(70, document.getElementById("typewriter-container"), "App|ETS", false, null)
     })
-}
-
-/**
- * For the android section, fade in objects when they are in the right position
- */
-window.onscroll = function () {
-    if(this.document.getElementById("blog").getBoundingClientRect().y < 0){
-        $('.navbar').css('background-color','rgba(0, 0, 0, 0.9)')
-    }else{
-        $('.navbar').css('background-color','rgba(0, 0, 0, 0.5)')
-    }
-    if(document.getElementById("club-description").getBoundingClientRect().y + document.getElementById("club-description").getBoundingClientRect().height/2 < 3 * window.innerHeight / 4){
-        $('#club-description').css("opacity","1")
-    }else{
-        $('#club-description').css("opacity","0")
-    }
-    if(document.getElementById("club-description").getBoundingClientRect().y + document.getElementById("club-description").getBoundingClientRect().height/2 < 1 * window.innerHeight / 4){
-        $('#club-description').css("opacity","0")
-    }
-
-    if(document.getElementById("club-mission").getBoundingClientRect().y + document.getElementById("club-mission").getBoundingClientRect().height/2 < 3 * window.innerHeight / 4){
-        $('#club-mission').css("opacity","1")
-    }else{
-        $('#club-mission').css("opacity","0")
-    }
-    if(document.getElementById("club-mission").getBoundingClientRect().y + document.getElementById("club-mission").getBoundingClientRect().height/2 < 1 * window.innerHeight / 4){
-        $('#club-mission').css("opacity","0")
-    }
-
-    if(document.getElementById("club-objectives").getBoundingClientRect().y < 3 * window.innerHeight / 4){
-        $('#club-objectives').css("opacity","1")
-    }else{
-        $('#club-objectives').css("opacity","0")
-    }
-    if(document.getElementById("club-objectives").getBoundingClientRect().y < 1 * window.innerHeight / 4){
-        $('#club-objectives').css("opacity","0")
-    }
 }
--- js/typewriter.js
+++ js/typewriter.js
@@ -6,49 +6,28 @@
  * @param {Bool} cursorToRemove Remove cursor at the end
  * @param {function} callback (Optional) The function to execute once the typing is done
  */
-var Typewriter = function (iterationSpeed, destinationElement, inputText, cursorToRemove, callback) {
+let Typewriter = function (iterationSpeed, destinationElement, inputText, cursorToRemove, callback) {
   
     iterateTypewriter = function (iterationSpeed, destinationElement, callback) {
         return function (inputText, iTextPos) {
-
-
             iArrLength = inputText.length
-
             iTextPos++
 
-
-
             destinationElement.innerText = inputText.substring(0, iTextPos);
+            
             if (iTextPos == iArrLength) {
-                if (cursorToRemove == true) {
-                    setTimeout("removeCursor()", 1000)
-                    if (callback != undefined) {
-                        callback()
-                    }
-                } else {
-                    if (callback != undefined) {
-                        callback()
-                    }
-                }
-
-
-
-
+                if (cursorToRemove == true)
+                    setTimeout("removeCursor()", 1000);
+                if (callback)
+                    callback();
             } else {
-
                 setTimeout("iterateTypewriter(\"" + inputText.replace("\"", "") + "\"," + iTextPos + ")", iterationSpeed);
-
-
             }
-
         }
-
-
-
     }(iterationSpeed, destinationElement, callback)
 
     removeCursor = function () {
-        destinationElement.classList.remove("typewriter")
+        destinationElement.classList.remove("typewriter");
     }
 
     setTimeout("iterateTypewriter(\"" + inputText.replace("\"", "") + "\"," + 0 + ")", 1000);
