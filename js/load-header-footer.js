b0ce3764c7b618d7720730485c6173686928f2c7
Louis-Philippe <louis.philippe.heon@gmail.com>
2024-12-22 13:11:13 -0500

    fix

--- js/load-header-footer.js
+++ js/load-header-footer.js
@@ -1,17 +1,7 @@
-async function loadContent(selector, url, action) {
-	const element = document.querySelector(selector);
-	const fetchedItem = await fetch(url);
-	const itemHtml = await fetchedItem.text();
-	element.innerHTML = itemHtml;
-	if (action) action();
-  }
-
-function addToggleNavbarEventListenner() {
+$("#header").load("components/header.html", function() {
 	document.querySelector("#nav-toggle-container").addEventListener("click", function () {
 		$("#nav-toggler").toggleClass("active");
 		$('#nav-toggle-content').slideToggle();
 	});
-}
-
-loadContent("#header", "components/header.html", null);
-loadContent("#footer", "components/footer.html", addToggleNavbarEventListenner);
+});
+$("#footer").load("components/footer.html"); 
